f195cc1722db92c86ddf2caef7a779d9a420f63a
arteman <arteman@parus.com.ua>
2017-04-06 16:02:23 +0300

    Trying to push files

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hell World!" );
+        System.out.println( "Helly World!" );
         System.out.println(Math.PI);
     }
 }
