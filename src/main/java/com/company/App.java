7bfd439b27363ef8866bf1ce8d0cff57118ed85f
arteman <arteman@parus.com.ua>
2017-04-06 16:49:57 +0300

    Changed 'p' value

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(Math.PI);
+        System.out.println(Math.PI*2);
     }
 }
